183c75b5e87fe63a195d89e179db8dd275ef4074
Ro Chernikov <imrhubris@gmail.com>
2022-08-22 07:32:42 +0300

    fix(test): test update

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-import Button from '../src/Button/Button';
+import Button from '../src/components/Button';
 
 describe('Button', () => {
   describe('should be rendered correctly', () => {
