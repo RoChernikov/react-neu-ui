3c508679d2fb138ee5964855a3a129a9b6b896dc
Ro Chernikov <imrhubris@gmail.com>
2022-08-22 07:30:55 +0300

    fix(export): export update

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import Button, { TButtonProps } from '../src/Button/Button';
+import Button, { TButtonProps } from './Button';
 
 const meta: Meta = {
   title: 'Welcome',
